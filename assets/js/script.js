e97f91f13a7d8fc099b37a3f1c740f1b294f6f4b
Ivana505 <ivana.iles@protonmail.com>
2021-10-12 00:03:19 +0000

    Add modal box for Choose your song

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,21 @@
+//initial click
+//var whiteKey = document.getElementsByClassName("whiteKey");
+
+//whiteKey.addEventListener("click" , function() { c1.style.background = "red"});
+
+//function to activate note sounds 
+
+//function to play notes from array
+
+
 // Get the modal
-var modal = document.getElementById("myModal");
+var modal = document.getElementById("howToPlayModal");
 
 // Get the button that opens the modal
 var btn = document.getElementById("how-to-play");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("how-to")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
@@ -23,3 +33,29 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 } 
+/*
+// Get the modal
+var modal = document.getElementById("choose");
+
+// Get the button that opens the modal
+var btn = document.getElementById("choose-your-song");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("your-song")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} */
