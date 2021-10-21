df7830465b5d73e2df0dafa69dedf5536496feca
Ivana505 <ivana.iles@protonmail.com>
2021-10-21 09:09:48 +0000

    Add JavaScript code to try and connect keys with songs

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,7 +25,6 @@ blackKeys.forEach((key) => {
 	});
 });
 
-
 //function to activate note sounds 
 function activateSound(key) {
 	const soundAudio = document.getElementById(key.dataset.sound);
@@ -44,56 +43,6 @@ function stopSound() {
 	  audio.pause();
   });
 }
-/*
-let maryHadLamb = ["e1","e1","c1","d1","e1","e1","e1","d1","d1","d1","e1","e1","e1","e1","d1","c1","d1","e1","e1","e1","d1","d1","e1","d1","c1"];
-
-function firstSong(start = 0, end = Infinity, step = 1) {
-    let maryHadLamb = start;
-    let iterationCount = 0;
-
-    const rangeIterator = {
-       next: function(firstSong) {
-           let result;
-           if (nextIndex < end) {
-               result = { value: nextIndex, done: false }
-               nextIndex += step;
-               iterationCount++;
-               return result;
-           }
-           return { value: iterationCount, done: true }
-       }
-    };
-    return rangeIterator;
-}
-
-
-/*song1=true;
-strictButton.addEventListener('click', (event) => {
-	if (stricButton.checked == true) {
-		strict=true;
-	}	else {
-			strict=false;
-		}
-})*/
-
-
-/*var maryHadLamb = new Audio(),
-    i = e1;
-var playlist = new Array("e1","e1","c1","d1","e1","e1","e1","d1","d1","d1","e1","e1","e1","e1","d1","c1","d1","e1","e1","e1","d1","d1","e1","d1","c1");
-
-
-
-/*let pressed = '';
-document.querySelectorAll('id').forEach(item => {
-  item.addEventListener('click', event => {
-    pressed += item.id;
-    if( pressed == '"e1","e1","c1"' ) {
-      alert("Success");
-    } else {
-      console.log(pressed);
-    }
-  })
-})*/
 
 let playNote = (key) => {
 	const noteSound = document.getElementById(key.dataset.note);
@@ -104,25 +53,32 @@ let playNote = (key) => {
 	  key.classList.remove('active');
 	});
   };
-  
+
+
+let songOne = ["e1","e1","c1","d1","e1","e1","e1","d1","d1","d1","e1","e1","e1","e1","d1","c1","d1","e1","e1","e1","d1","d1","e1","d1","c1"]
+for (let i = 0; i < songOne.length; i++) {
+    console.log(songOne[i]);
+}
+function songOne1 () {
+	const songSound = document.getElementById("song1");
+	soundAudio.currentTime = 0;
+	songOne();
+	soundAudio.play();
+}
+
 
 /*
-function melody(x) {
-  y += x;
-  if (maryHadLamb == "e1" ||
-    y == "e1e1" ||
-    y == "e1e1c1" ||
-    y == "e1e1c1" ||
-    y == "e1e1c1" ||
-    y == "e1e1c1" ||
-    y == "e1e1c1"
-  ) {
-    if (y == "e1e1c1") {
-      alert("Success!");
+let pressed = '';
+document.querySelectorAll('id').forEach(item => {
+  item.addEventListener('click', event => {
+    pressed += item.id;
+    if( pressed == '"e1","e1"' ) {
+      alert("Success");
+    } else {
+      console.log(pressed);
     }
-  } else y = "";
-}
-*/
+  })
+})*/
 
 /*function playMaryHadLamb() {
     let audio = document.getElementsByTagName('audio');
@@ -141,15 +97,11 @@ document.addEventListener('click', e => {
 	if (maryHadLambIndex > -1) playNote(maryHadLamb[MARY_HAD_LAMB])
 })*/
 
-
 //const maryHadLamb = ["e1","e1","c1","d1","e1","e1","e1","d1","d1","d1","e1","e1","e1","e1","d1","c1","d1","e1","e1","e1","d1","d1","e1","d1","c1"];
 //for (let i = 0; i < maryHadLamb.length; i++) {
 
-
-
   //console.log(maryHadLamb[i]);
 //}
-
 
 /*
 const twinkleTwinkle = ["c1","c1","g1","g1","a1","a1","g1","f1","f1","e1","e1","d1","d1","c1","g1","g1","f1","f1","e1","e1","d1","g1","g1","f1","f1","e1",
@@ -163,8 +115,6 @@ const jingleBells = ["e1","e1","e1","e1","e1","e1","e12,"g1","c1","d1","e1","f1"
 for (let i = 0; i < jingleBells.length; i++) {
     console.log(jingleBells[i]);
 }*/
-
-
 
 // Get the modal
 var modal = document.getElementById("howToPlayModal");
